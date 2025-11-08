b24cfeb0ff9b95fee073a17eeef5c19cbb5b7b88
abh80 <abhb787@outlook.com>
2025-11-08 17:27:32 +0530

    add: detailed documentation for core Step model and metadata utilities

--- modules/core/src/main/scala/com/verschraenkt/ci/core/model/Step.scala
+++ modules/core/src/main/scala/com/verschraenkt/ci/core/model/Step.scala
@@ -2,6 +2,22 @@ package com.verschraenkt.ci.core.model
 
 import cats.data.{ NonEmptyList, NonEmptyVector }
 
+/** Metadata configuration for pipeline steps
+  * @param id
+  *   Optional identifier for the step
+  * @param when
+  *   Optional condition for when this step should run
+  * @param timeoutSec
+  *   Optional timeout in seconds for the step execution
+  * @param continueOnError
+  *   Whether to continue pipeline execution if this step fails
+  * @param retry
+  *   Optional number of times to retry this step if it fails
+  * @param env
+  *   Environment variables to set for this step
+  * @param workingDirectory
+  *   Optional working directory for this step
+  */
 final case class StepMeta(
     id: Option[String] = None,
     when: Option[String] = None,
@@ -12,25 +28,38 @@ final case class StepMeta(
     workingDirectory: Option[String] = None
 )
 
+/** Trait for steps that have metadata */
 sealed trait HasMeta:
   def meta: StepMeta
 
+/** Pipeline execution steps */
 enum Step derives CanEqual:
-  case Checkout()(using val meta: StepMeta)                extends Step with HasMeta
+  /** Checkout source code from version control */
+  case Checkout()(using val meta: StepMeta) extends Step with HasMeta
+
+  /** Run a command */
   case Run(command: CommandLike)(using val meta: StepMeta) extends Step with HasMeta
+
+  /** Restore files from cache */
   case RestoreCache(cache: CacheLike, paths: NonEmptyList[String])(using val meta: StepMeta)
       extends Step
       with HasMeta
+
+  /** Save files to cache */
   case SaveCache(cache: CacheLike, paths: NonEmptyList[String])(using val meta: StepMeta)
       extends Step
       with HasMeta
+
+  /** Composite step containing multiple steps */
   case Composite(steps: NonEmptyVector[Step]) extends Step
 
 extension (step: Step)
+  /** Get the metadata for this step if it has any */
   def getMeta: Option[StepMeta] = step match
     case h: HasMeta => Some(h.meta)
     case _          => None
 
+  /** Create a copy of this step with modified metadata */
   def withMeta(f: StepMeta => StepMeta): Step = step match
     case s: HasMeta =>
       s match
@@ -39,4 +68,3 @@ extension (step: Step)
         case rc: Step.RestoreCache => rc.copy()(using f(rc.meta))
         case sc: Step.SaveCache    => sc.copy()(using f(sc.meta))
     case c: Step.Composite => c
-
