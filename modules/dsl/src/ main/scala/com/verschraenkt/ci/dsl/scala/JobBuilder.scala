c519e185d86d1c722fbed2522cf90a1026785618
abh80 <abhb787@outlook.com>
2025-11-10 00:21:32 +0530

    add: job builder

--- modules/dsl/src/ main/scala/com/verschraenkt/ci/dsl/scala/JobBuilder.scala
+++ modules/dsl/src/ main/scala/com/verschraenkt/ci/dsl/scala/JobBuilder.scala
@@ -2,15 +2,4 @@ package com.verschraenkt.ci.dsl.scala
 
 import com.verschraenkt.ci.core.model.*
 
-final case class JobBuilder(
-    id: JobId,
-    steps: NonEmptyVector[Step],
-    dependencies: Set[JobId] = Set.empty,
-    resources: Resource = Resource(1000, 512, 0, 0),
-    timeout: FiniteDuration,
-    matrix: Map[String, NonEmptyVector[String]] = Map.empty,
-    container: Option[Container],
-    labels: Set[String] = Set.empty,
-    concurrencyGroup: Option[String] = None,
-    condition: Condition = Condition.Always
-)
+final case class JobBuilder(name: String, steps: Vector[Step]) {}
