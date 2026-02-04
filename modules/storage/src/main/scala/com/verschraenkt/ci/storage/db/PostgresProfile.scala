fb38b1707b251591d8e749836a4fd63e356cc2ef
abh80 <abhb787@outlook.com>
2026-02-04 19:36:04 +0530

    feat: enhance Postgres profile

--- modules/storage/src/main/scala/com/verschraenkt/ci/storage/db/PostgresProfile.scala
+++ modules/storage/src/main/scala/com/verschraenkt/ci/storage/db/PostgresProfile.scala
@@ -1,11 +1,29 @@
 package com.verschraenkt.ci.storage.db
 
 import com.github.tminglei.slickpg.*
+import slick.basic.Capability
+import slick.jdbc.JdbcCapabilities
 
-trait MyPostgresProfile extends ExPostgresProfile with PgArraySupport:
+/** Enhanced PostgreSQL profile with support for:
+  *   - PostgreSQL ENUM types
+  *   - JSONB (manual mapping via ColumnTypes)
+  *   - Array types
+  *   - java.time types (Instant, LocalDateTime, etc.)
+  */
+trait MyPostgresProfile
+    extends ExPostgresProfile
+    with PgArraySupport
+    with PgEnumSupport
+    with PgDate2Support:
+
+  override protected def computeCapabilities: Set[Capability] =
+    super.computeCapabilities + JdbcCapabilities.insertOrUpdate
 
   override val api = MyAPI
 
-  object MyAPI extends ExtPostgresAPI with ArrayImplicits
+  object MyAPI
+      extends ExtPostgresAPI
+      with ArrayImplicits
+      with Date2DateTimeImplicitsDuration
 
 object PostgresProfile extends MyPostgresProfile
