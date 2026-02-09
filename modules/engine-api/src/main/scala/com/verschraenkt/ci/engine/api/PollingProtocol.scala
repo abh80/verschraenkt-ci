460224c8bf66f36af48b5fb195c609b77228e34f
abh80 <abhb787@outlook.com>
2026-02-09 18:40:12 +0530

    chore: add copyright headers

--- modules/engine-api/src/main/scala/com/verschraenkt/ci/engine/api/PollingProtocol.scala
+++ modules/engine-api/src/main/scala/com/verschraenkt/ci/engine/api/PollingProtocol.scala
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2025 abh80 on gitlab.com. All rights reserved.
+ *
+ * This file and all its contents are originally written, developed, and solely owned by abh80 on gitlab.com.
+ * Every part of the code has been manually authored by abh80 on gitlab.com without the use of any artificial intelligence
+ * tools for code generation. AI assistance was limited exclusively to generating or suggesting comments, if any.
+ *
+ * Unauthorized use, distribution, or reproduction is prohibited without explicit permission from the owner.
+ * See License
+ */
 package com.verschraenkt.ci.engine.api
 
 import com.verschraenkt.ci.core.model.JobId
