61fc2e83459389ca88d8857fcfe3dfc7326f4f7c
abh80 <abhb787@outlook.com>
2026-02-12 21:53:28 +0530

    refactor: fixed infnite recursion error

--- modules/storage/src/main/scala/com/verschraenkt/ci/storage/util/TableCast.scala
+++ modules/storage/src/main/scala/com/verschraenkt/ci/storage/util/TableCast.scala
@@ -7,23 +7,26 @@ import com.verschraenkt.ci.storage.errors.StorageError
 import org.postgresql.util.PSQLException
 
 trait TableCast[R]:
+  self =>
+
   protected def table: TableQuery[? <: Table[R]]
 
-  trait InsertAction[D, M]:
+  abstract class InsertAction[D, M]:
     protected def mapper: DomainRowMapper[D, R, M]
     protected def entityName: String
     protected def getId(domain: D): String
 
     protected def transactionally[T](action: DBIO[T]): IO[T]
     protected def fail[T](error: StorageError)(using applicationContext: ApplicationContext): IO[T]
-    protected def isDuplicateKeyError(e: PSQLException): Boolean =
+
+    protected final def isDuplicateKeyError(e: PSQLException): Boolean =
       e.getSQLState == "23505"
 
-    def insert(domain: D, metadata: M)(using enc: io.circe.Encoder[D])(using
+    final def insert(domain: D, metadata: M)(using enc: io.circe.Encoder[D])(using
         applicationContext: ApplicationContext
     ): IO[D] =
       val row          = mapper.fromDomain(domain, metadata)
-      val insertAction = table += row
+      val insertAction = self.table += row
 
       transactionally(insertAction)
         .map(_ => domain)
