fe26c2c148fd8da8e427a84cb7f0435051592610
abh80 <abhb787@outlook.com>
2026-02-12 20:58:01 +0530

    feat: add DomainRowMapper trait for mapping between domain models and database rows

---
+++ modules/storage/src/main/scala/com/verschraenkt/ci/storage/util/DomainRowMapper.scala
@@ -0,0 +1,10 @@
+package com.verschraenkt.ci.storage.util
+
+import io.circe.{Decoder, Encoder}
+
+trait DomainRowMapper[Domain, Row, Metadata]:
+  /** Convert domain model to database row */
+  def fromDomain(domain: Domain, metadata: Metadata)(using Encoder[Domain]): Row
+
+  /** Convert database row to domain model */
+  def toDomain(row: Row)(using Decoder[Domain]): Either[io.circe.Error, Domain]
